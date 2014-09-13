d15bbe4d906b22c5027a6760bbc7bbe157bb9a18
loki2302 <loki2302@loki2302.me>
2014-09-13 19:03:47 +0400

    ResourceRouteHandler is now configurable

--- src/main/java/me/loki2302/DummyModule.java
+++ src/main/java/me/loki2302/DummyModule.java
@@ -2,6 +2,7 @@ package me.loki2302;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
+import me.loki2302.framework.handling.ResourceRouteHandler;
 import me.loki2302.framework.results.HandlerResultProcessorRegistry;
 import me.loki2302.framework.results.is.InputStreamHandlerResultProcessor;
 import me.loki2302.framework.results.mav.ModelAndViewHandlerResultProcessor;
@@ -25,5 +26,7 @@ public class DummyModule extends AbstractModule {
         bind(ModelAndViewHandlerResultProcessor.class).asEagerSingleton();
         bind(InputStreamHandlerResultProcessor.class).asEagerSingleton();
         bind(HandlerResultProcessorRegistry.class).asEagerSingleton();
+
+        bind(ResourceRouteHandler.class).toInstance(new ResourceRouteHandler("path", "/assets"));
     }
 }
---
+++ src/main/java/me/loki2302/framework/handling/ResourceRouteHandler.java
@@ -0,0 +1,25 @@
+package me.loki2302.framework.handling;
+
+import me.loki2302.framework.handling.RouteHandler;
+
+import java.util.Map;
+
+public class ResourceRouteHandler implements RouteHandler {
+    private final String pathContextVariableName;
+    private final String root;
+
+    public ResourceRouteHandler(String pathContextVariableName, String root) {
+        this.pathContextVariableName = pathContextVariableName;
+        this.root = root;
+    }
+
+    @Override
+    public Object handle(Map<String, Object> context) {
+        if(!context.containsKey(pathContextVariableName)) {
+            throw new RuntimeException("Context doesn't have " + pathContextVariableName);
+        }
+
+        String relativePath = (String)context.get(pathContextVariableName);
+        return getClass().getResourceAsStream(root + relativePath);
+    }
+}
