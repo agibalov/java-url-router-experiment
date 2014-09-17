f20f0914a6018e437c74adb604cd1af6000600a9
loki2302 <loki2302@loki2302.me>
2014-09-17 08:58:49 +0400

    Removed old route matcher

--- framework/src/main/java/me/loki2302/routing/naive/RouteSegment.java
+++
@@ -1,8 +0,0 @@
-package me.loki2302.routing.naive;
-
-import java.util.Map;
-
-public interface RouteSegment {
-    SegmentMatchResult match(String urlSegment);
-    String getValue(Map<String, Object> context);
-}
